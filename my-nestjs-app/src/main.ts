b9677a45dbea83e9d7a25bda767bd1463eb593c6
Loading <83232326+Loading@users.noreply.github.com>
2024-10-30 12:06:41 +0100

    small fixes
    lol

--- my-nestjs-app/src/main.ts
+++ my-nestjs-app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+  );
+
+  await app.listen(3001);
 }
 bootstrap();
