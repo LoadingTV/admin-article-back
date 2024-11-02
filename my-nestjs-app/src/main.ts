b591edc8147dd97017141a54aae4ac8efc4a12b7
Loading <83232326+Loading@users.noreply.github.com>
2024-11-02 22:54:41 +0100

    GENERAL FIX Log&Reg

--- my-nestjs-app/src/main.ts
+++ my-nestjs-app/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
   });
 
   await app.listen(3001);
